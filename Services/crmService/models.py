de092851b139dff32436a6774e8fbb103573d859
Tworzyk <115981677+Tworzyk@users.noreply.github.com>
2026-01-18 14:18:40 +0100

    Delete Services directory

--- Services/crmService/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, Float
-from common import Base
-
-class CustomerLoyalty(Base):
-    __tablename__ = "customer_loyalty"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, unique=True, index=True)
-    points = Column(Float, default=0.0)
